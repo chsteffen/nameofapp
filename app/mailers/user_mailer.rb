d6cc927a584a81b410f7c5b174ca760c3b3472e5
Caitie <caitie.steffen@gmail.com>
2016-08-07 13:40:13 -0700

    fixed user.mailer.rb to field

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
 	default from: "caitie.steffen@gmail.com"
-	
+
 	def contact_form(email, name, message)
 	@message = message
 		mail(:from => email,
-			:to => 'your-email@example.com',
+			:to => 'caitie.steffen@gmail.com',
 			:subject => "A new contact form message from #{name}")
 	end
 end
