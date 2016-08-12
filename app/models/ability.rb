623a3032000013245d5a078ad3acefb8d7ed6daa
Caitie <caitie.steffen@gmail.com>
2016-08-11 21:48:03 -0700

    changed some admin features with cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    can :destroy, Comment
+    if user.admin?
+    	can :manage, :all, User, id: user.id
+    	can :destroy, Comment
+    else 
+    	can :read, :all
+    end
   end
 
   
