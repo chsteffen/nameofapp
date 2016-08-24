cdc8ccb3c14ace5de5de50d73bd2d5f4f8294e1c
Caitie <caitie.steffen@gmail.com>
2016-08-23 22:34:06 -0700

    defined products columns changes

--- db/migrate/20160824040120_remove_price_in_cents_from_products.rb
+++ db/migrate/20160824040120_remove_price_in_cents_from_products.rb
@@ -1,4 +1,5 @@
 class RemovePriceInCentsFromProducts < ActiveRecord::Migration
   def change
+  	remove_column :products, :price_in_cents
   end
 end
--- db/migrate/20160824042058_change_price_for_products.rb
+++ db/migrate/20160824042058_change_price_for_products.rb
@@ -1,4 +1,5 @@
 class ChangePriceForProducts < ActiveRecord::Migration
   def change
+  	rename_column :products, :price, :price_in_cents
   end
 end
