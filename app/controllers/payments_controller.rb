6ff9dd12172c957c212f624b8f24354499ac1b32
Caitie <caitie.steffen@gmail.com>
2016-08-26 11:49:53 -0700

    declared values for order total, user_id and product_id

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,10 +1,9 @@
 class PaymentsController < ApplicationController
 
 def create
-	def find
-		@product = Product.find(params[:product_id])
-		@user = User.new(user_params)
-	end
+	@product = Product.find(params[:product_id])
+	@user = User.new(user_params)
+end
 	token = params[:stripeToken]
 	# Create the charge on Stripe's servers - this will charge the user's card
 	begin
@@ -16,7 +15,7 @@ def create
 			)
 
 		if charge.paid
-			Order.create(:product_id, :user_id, :total, :email)
+			Order.create(:product_id => @product.id, :user_id => @user.id, :total => @product.price, :email)
 		end
 
 		rescue Stripe::CardError => e
