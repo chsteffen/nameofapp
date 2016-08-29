e199d56c25c34e3649a28087dd19f27f490c85e4
Caitie <caitie.steffen@gmail.com>
2016-08-28 21:52:26 -0700

    AJAX requests for database

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,12 +1,14 @@
-$(document).on('ready page:load', function() {
-	$('.img-zoom').elevateZoom();
+var refreshRating = function() {
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets',
 		readOnly: true,
 		score: function() {
 			return $(this).attr('data-score');
 		}
-		
 	});
+};
+
+$(document).on('ready page:load', function() {
+	$('.img-zoom').elevateZoom();
 
 });
